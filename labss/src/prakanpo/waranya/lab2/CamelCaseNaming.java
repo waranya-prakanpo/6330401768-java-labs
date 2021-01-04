1537b606be65ce877e6f5b52a2108f631310a933
waranya-prakanpo <76422023+waranya-prakanpo@users.noreply.github.com>
2021-01-04 20:55:03 +0700

    Add files via upload

---
+++ labss/src/prakanpo/waranya/lab2/CamelCaseNaming.java
@@ -0,0 +1,28 @@
+/**
+ * This Athlete program is to accept three arguments: athlete name, the sport name
+ *  that that athlete plays, and the nationality of that athlete.  Its output format is “My
+ *  favorite athlete is <athlete_name> who plays <sport_name> and has
+ * nationality as <athlete_nationality>”
+ *
+ * Author: Waranya Prakanpo
+ * ID: 633040176-8
+ * Sec: 1
+ * Date: January 4, 2021
+ *
+ **/
+package prakanpo.waranya.lab2;
+public class CamelCaseNaming {
+    public static void main(String[] args) {
+        if (args.length == 2) {
+            String first = args[0];
+            String second = args[1];
+            String firstedit = first.substring(0,1).toUpperCase() + first.substring(1).toLowerCase();
+            String secondedit = second.substring(0,1).toUpperCase() + second.substring(1).toLowerCase();
+            System.out.println("The first word is " + args[0]);
+            System.out.println("The Second word is " + args[1]);
+            System.out.println("The concatenate with cemal case is " + firstedit+secondedit);
+        } else {
+            System.err.println("prakanpo.waranya.lab2.CamelCaseNaming : <First world> <Second word>");
+        }
+    }
+}
---
+++ labss/src/prakanpo/waranya/lab2/patient.java
@@ -0,0 +1,24 @@
+/**
+ * This Athlete program is to accept three arguments: athlete name, the sport name
+ *  that that athlete plays, and the nationality of that athlete.  Its output format is “My
+ *  favorite athlete is <athlete_name> who plays <sport_name> and has
+ * nationality as <athlete_nationality>”
+ *
+ * Author: Waranya Prakanpo
+ * ID: 633040176-8
+ * Sec: 1
+ * Date: January 4, 2021
+ *
+ **/
+package prakanpo.waranya.lab2;
+public class patient {
+    public static void main(String[] args) {
+        if (args.length == 3) {
+            System.out.println("Patient's name is " + args[0]);
+            System.out.println("Patient's age is " + args[1]);
+            System.out.println(args[0] + " comes from " + args[2]);
+        } else {
+            System.err.println("prakanpo.waranya.lab2.patient <prakanpo.waranya.lab2.patient name> <prakanpo.waranya.lab2.patient age> <country>");
+        }
+    }
+}
