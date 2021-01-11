168239da3c254603097d0e4c78b13eeb0d5d762b
waranya-prakanpo <76422023+waranya-prakanpo@users.noreply.github.com>
2021-01-11 14:33:25 +0700

    upload lab3

---
+++ labss/src/prakanpo/waranya/lab3/GuessNumberGame.java
@@ -0,0 +1,25 @@
+package prakanpo.waranya.lab3;
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessNumberGame {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int value = rand.nextInt((10 - 1) + 1) + 1;
+        int maxTries = 3;
+        int a;
+        for (a = 1; a <= maxTries; a++) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Please enter a guess (1-10):");
+            int i = sc.nextInt();
+            if (i < value) {
+                System.out.println("Please type a higher number ! Number of remaining tries:" + (maxTries - a));
+            } else if (i > value) {
+                System.out.println("Please type a lower number ! Number of remaining tries:" + (maxTries - a));
+            } else {
+                System.out.println("Congratulations!  That’s correct");
+                break;
+            }
+        }
+    }
+}
